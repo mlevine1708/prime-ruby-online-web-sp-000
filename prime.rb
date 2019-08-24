7c1ab19756c55473b845ff6bbd7f94216b693918
Micah Levine <micah.levine@outlook.com>
2019-08-24 00:11:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,6 @@
 # Add  code here!
+require 'prime'
+
 def prime?(num)
-  start = 2
-  if number > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test|
-      number % num_to_test == 0
-    end
-  else
-    false
-  end
+    num.prime?
 end
