2ab0d2d4b6cd6c795fdf75d62c61b55456bf6e7f
Micah Levine <micah.levine@outlook.com>
2019-08-24 00:01:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
+
 def prime?(num)
-  (2..Math.sqrt(num)).each { |i|
-   if num % i == 0 && i < num
-     return false
-   end
-   }
-   true
+    2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    end
+    return false
 end
