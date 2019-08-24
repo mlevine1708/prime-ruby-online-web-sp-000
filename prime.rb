33c7c2899029daa8d4343ae361114882e7f03c47
Micah Levine <micah.levine@outlook.com>
2019-08-24 00:01:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(num)
     2.upto(Math.sqrt(num).ceil) do |i|
         break if num%i==0
-        return true if i==Math.sqrt(num).ceil   
+        return true if i==Math.sqrt(num).ceil
     end
     return false
 end
