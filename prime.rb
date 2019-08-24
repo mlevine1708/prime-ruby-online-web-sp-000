9be1e08c4420efb5be9773ac0f3a0466a3a00d34
Micah Levine <micah.levine@outlook.com>
2019-08-24 00:05:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 # Add  code here!
 def prime?(num)
-  2.upto(Math.sqrt(n)) do |x|
-    if n % x == 0
-      return false
-    else
-      return true
-    end
+  (2..(num - 1)).each do |n|
+    return false if num % n == 0
   end
+  true
 end
